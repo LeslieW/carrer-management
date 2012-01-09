1061c9386ecce92139a4cb511da42209875c54cc
rikajff@gmail.com <rikajff@gmail.com@867bdf94-be0c-e0be-5d8a-5ffa51094a64>
2012-01-09 14:22:58 +0000

    initial draft

---
+++ trunk/include/version.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *
+ *	version.h
+ *
+ *	Created at:
+ *	2012-01-09
+ *
+ *	Author(s):
+ *
+ *		B5-8 LAPRIII group
+ *
+ *****************************************************************************/
+ 
+ #ifndef _VERSION_H
+ #define _VERSION_H
+ 
+ #define APP_NAME					"Career Management"
+ #define APP_VERSION				"0.0.1"
+ 
+ #endif // _VERSION_H
